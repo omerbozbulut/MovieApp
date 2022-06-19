584b1797dbe07466fa7fca23829e0cde86625cf1
omer <omerbzblt613@gmail.com>
2022-06-19 15:01:52 +0300

    init

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by omer faruk bozbulut on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
