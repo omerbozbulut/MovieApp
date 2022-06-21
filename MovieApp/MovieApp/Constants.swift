cc69a00afccb7debd51dc94b8cadd0a172926e54
omer <omerbzblt613@gmail.com>
2022-06-21 17:07:06 +0300

    update constants

---
+++ MovieApp/MovieApp/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by omer faruk bozbulut on 21.06.2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let animationName = "developer"
+    struct Fonts{
+        static let TrebuchetMSBold = "TrebuchetMS-Bold"
+        static let helvetica = "Helvetica"
+    }
+    
+    struct Titles {
+        static let splashScreenLabel = "M O V İ E S"
+    }
+}
