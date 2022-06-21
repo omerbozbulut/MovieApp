278027b6aa5e25f4e43771b83ccfb4622b125664
omer <omerbzblt613@gmail.com>
2022-06-21 18:48:21 +0300

    Movie model

---
+++ MovieApp/MovieApp/Models/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by omer faruk bozbulut on 21.06.2022.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let title: String
+    let overview: String
+    let vote_average: Float
+    let release_date: String
+    let poster_path: String
+}
